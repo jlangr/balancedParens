cef643e4fe1e3c324dd21db10eeddd8ed384dc9d
Jeff Langr <jeff.langr@outpace.com>
2017-04-14 14:56:23 -0600

    one solution

--- src/Parser.java
+++ src/Parser.java
@@ -1,5 +1,38 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class Parser {
+    Map<Character,Character> closingCharToOpeningChar = new HashMap<>();
+    {
+        closingCharToOpeningChar.put('}', '{');
+        closingCharToOpeningChar.put(')', '(');
+    }
+
     public boolean isBalanced(String expression) {
-        throw new RuntimeException("not yet implemented");
+        int[] depthsByOpeningChar = new int[255];
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (isOpeningChar(c)) {
+                stack.push(c);
+                depthsByOpeningChar[c]++;
+            }
+            else {
+                char last = stack.isEmpty() ? 0 : stack.peek();
+                if (last != matchingOpeningChar(c))
+                    return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private Character matchingOpeningChar(char c) {
+        return closingCharToOpeningChar.get(c);
+    }
+
+    private boolean isOpeningChar(char c) {
+        return closingCharToOpeningChar.values().contains(c);
     }
 }
--- src/ParserTest.java
+++ src/ParserTest.java
@@ -13,39 +13,59 @@ public class ParserTest {
         assertTrue(parser.isBalanced(""));
     }
 
-    @Ignore
     @Test
     public void unbalanced_with_only_left_paren() {
         assertFalse(parser.isBalanced("("));
     }
 
-    @Ignore
     @Test
     public void balanced_with_left_and_right_paren() {
         assertTrue(parser.isBalanced("()"));
     }
 
-    @Ignore
     @Test
     public void not_balanced_with_left_and_right_paren_reversed() {
         assertFalse(parser.isBalanced(")("));
     }
 
-    @Ignore
     @Test
     public void not_balanced_with_differnt_left_and_right_paren() {
         assertFalse(parser.isBalanced("(}"));
     }
 
-    @Ignore
     @Test
     public void balanced_with_LL_RR_paren() {
         assertTrue(parser.isBalanced("(())"));
     }
 
-    @Ignore
     @Test
-    public void not_balanced_with_LRR_paren() {
-        assertFalse(parser.isBalanced("())"));
+    public void not_balanced_with_LLLR_paren() {
+        assertFalse(parser.isBalanced("((()"));
+    }
+
+    // Passes already
+    @Test
+    public void balanced_with_LRLR_paren() {
+        assertTrue(parser.isBalanced("()()"));
+    }
+
+    @Test
+    public void not_balanced_with_brace_mismatch() {
+        assertFalse(parser.isBalanced("{"));
+    }
+
+    @Test
+    public void not_balanced_when_types_cross() {
+        assertFalse(parser.isBalanced("({)}"));
+    }
+
+    @Test
+    public void not_balanced_when_types_cross_2() {
+        assertFalse(parser.isBalanced("{(})"));
+    }
+
+    @Test
+    public void balanced_with_mixed_types() {
+        assertTrue(parser.isBalanced("{({}())}"));
     }
 }
